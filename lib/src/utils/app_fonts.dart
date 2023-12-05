d385bdfb24ddfda39f73c352ea1ecf4a36206c86
mostafamahmoud145 <77023213+mostafamahmoud145@users.noreply.github.com>
2023-12-05 16:17:14 +0200

    push

---
+++ lib/src/utils/app_fonts.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// App fonts weight
+class AppFontsWeightManager {
+  static const FontWeight bold = FontWeight.bold;
+  static const FontWeight bold300 = FontWeight.w300;
+  static const FontWeight bold500 = FontWeight.w500;
+  static const FontWeight bold700 = FontWeight.w700;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight bold100 = FontWeight.w100;
+}
+
+/// App fonts sizes
+class AppFontsSizeManager {
+  static const double s9 = 9.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s19 = 19.0;
+  static const double s11 = 11.0;
+  static const double s15 = 15.0;
+  static const double s12 = 12.0;
+  static const double s21 = 21.0;
+  static const double s20 = 20.0;
+  static const double s14 = 14.0;
+  static const double s13 = 13.0;
+  static const double s21_3 = 21.3;
+  static const double s14_5 = 14.5;
+  static const double s13_5 = 13.5;
+  static const double s18_6 = 18.6;
+  static const double s32 = 32;
+  static const double s20_6 = 20.6;
+  static const double s26_6 = 26.6;
+  static const double s26 = 26;
+  static const double s24 = 24;
+  static const double s25 = 25.0;
+  static const double s17_5 = 17.5;
+  static const double s12_5 = 12.5;
+  static const double s28 = 28;
+  static const double s22_5 = 22.5;
+  static const double s10 = 10;
+  static const double s35 = 35;
+  static const double s30 = 30;
+}
---
+++ lib/src/utils/colorsFile.dart
@@ -0,0 +1,110 @@
+import 'dart:ui';
+
+
+class AppColors {
+  static const white1 = Color.fromRGBO(255, 255, 255, 1.0);
+  static const warmPurple3 = Color.fromRGBO(156, 57, 129, 0.33);
+  static const grey = Color.fromRGBO(167, 165, 165, 1.0);
+  static const grey3 = Color.fromRGBO(175, 175, 175, 1.0);
+  static const darkGrey2 = Color.fromRGBO(175, 175, 175, 024);
+  static const darkGrey3 = Color.fromRGBO(112, 112, 112, 1);
+  static const lightgrey5 = Color.fromRGBO(246, 246, 246, 1);
+
+  static const grey4 = Color.fromRGBO(228, 225, 225, 1.0);
+  static const grey6 = Color.fromRGBO(224, 224, 224, 1.0);
+  static const grey7 = Color.fromRGBO(184, 180, 180,1);
+  static const greyShade400 = Color.fromRGBO(189, 189, 189,1);
+
+  static const greenShade500 = Color.fromRGBO(76, 175, 80,1);
+  static const redShade500 = Color.fromRGBO(244, 67, 54,1);
+
+
+  static const white3 = Color.fromRGBO(237, 237, 237, 1.0);
+  static const white2 = Color.fromRGBO(250, 250, 250, 1.0);
+  static const white4 = Color.fromRGBO(240, 240, 240, 1.0);
+  static const white5 = Color.fromRGBO(227, 227, 227, 1.0);
+  static const white6 = Color.fromRGBO(247, 247, 247, 1.0);
+
+  static const warmGrey = Color.fromRGBO(158, 158, 158, 1.0);
+  static const black1 = Color.fromRGBO(35, 35, 35, 1.0);
+  static const lightPurple = Color.fromRGBO(246, 212, 237, 0.37);
+  static const pureBlack = Color.fromRGBO(0, 0, 0, 1);
+  static const lightBlack = Color.fromRGBO(32, 32, 32, 1);
+
+  static const Sign_out_title = Color.fromRGBO(29, 27, 32, 1);
+  static const Sign_out_Subtitle = Color.fromRGBO(73, 69, 79, 1);
+  static const Sign_out = Color.fromRGBO( 215, 82, 82, 1);
+  static const Sign_out2 = Color.fromRGBO(0, 0, 0, 1);
+  static const warmPurple4 = Color.fromRGBO(123, 108, 150, 1);
+  static const grey8 = Color.fromRGBO(112, 112, 112, 0.28);
+  static const green2 = Color.fromRGBO(165, 215, 82, 1);
+  static const lightPink3 = Color.fromRGBO(246, 212, 237, 1);
+  static const lightGrey5 = Color.fromRGBO(199, 198, 198, 1);
+  static const lightGrey6 = Color.fromRGBO(236, 236, 236, 0.65);
+  static const lightGrey7 = Color.fromRGBO(236, 236, 236, 1);
+
+
+
+  static const warmPurple = Color.fromRGBO(156, 57, 129, 0.18);
+  static const pinkShadowColor = Color.fromRGBO(187, 48, 149, 0.14);
+  static const warmPurple2 = Color.fromRGBO(126, 57, 129, 0.18);
+  static const linear2 = Color.fromRGBO(156, 57, 129, 1.0);
+  static const pink1 = Color.fromRGBO(215, 82, 82, 1);
+  static const greenButton = Color.fromRGBO(131, 227, 57, 1.0);
+  static const linear11 = Color.fromRGBO(191 ,72 ,159, 1);
+  static const linear12 = Color.fromRGBO(209, 105, 180, 1);
+  static const linear3 = Color.fromRGBO(156, 57, 129, 1);
+  static const yellow2 = Color.fromRGBO(255,245 ,0 ,1);
+  static const yellow3 = Color.fromRGBO(255, 213, 8, 1);
+
+  static const appbartext =Color.fromRGBO(30, 30, 30, 1.0);
+  static const black3 =Color.fromRGBO(50, 47, 53, 1);
+  static const black4 =Color.fromRGBO(32, 32, 32, 1);
+  static const red1 =Color.fromRGBO(234, 33, 33, 1);
+  static const redShade700 =Color.fromRGBO(211, 47, 47, 1);
+
+  static const pink3 =Color.fromRGBO(156, 57 ,129, 1);
+  static const darkGrey =Color.fromRGBO(147, 147, 147, 1);
+
+  static const orangeAccent =Color.fromRGBO(255, 171, 64, 1);
+
+
+
+
+  static const green = Color(0xFFD8FABE);
+  static const yellow = Color(0xFFFFD503);
+  static const black = Color(0xFF282B30);
+  static const white = Color(0xFFFFFFFF);
+  static const pink = Color(0xFF9D3A82);
+  static const pink2 = Color(0xFF9C3981);
+  static const greyDark = Color(0xFF9E9E9E);
+  static const lightGrey = Color(0xFFECECEC);
+  static const lightGrey2 = Color(0xFFfafafa);
+  static const lightGrey3 = Color(0xfff2f2f2);
+  static const lightGrey4 = Color(0xffb8b4b4);
+  static const lightPink = Color(0xFFF6D4ED);
+  static const lightPink2 = Color(0xFFFAF5F9);
+  static const shareColor = Color(0xFFF6D4DE);
+  static const moveColor = Color(0xFFE6D9FC);
+  static const linear1 = Color(0xffb93795); //(0xFFD169B9);
+  static const red = Color(0xFFFF0000);
+  static const lightGrey1 = Color(0xffafafaf);
+  static const black2 = Color(0xff202020);
+  static const grey5 = Color(0xff747474);
+  static const blue = Color(0xFF3b98e1);
+  static const grey9 = Color(0xFFF8F8F8);
+
+
+  //static Color separatorColor = HexColor('ECECEC');
+  static const lightGray =Color.fromRGBO(211, 211, 211, 1);
+  static const gradiant1 =Color.fromRGBO(134,57,114,1);
+  static const gradiant2 =Color.fromRGBO(209, 105, 180,1);
+  static const lightGrey8 = Color.fromRGBO(248, 248, 248,1);
+  static const grey_dark = Color.fromRGBO(32, 32, 32,1);
+  static const grey_light = Color.fromRGBO(239, 240, 246,1);
+  static const surveyShadow = Color.fromRGBO(0, 0, 0,0.08);
+  static const formFieldColor = Color.fromRGBO(249, 249, 249,1);
+
+
+
+}
